02534ba3a604f4fefbb7149a03a1fd3f084162e3
Wolf van der Hert <thelegendofwolfi@gmail.com>
2021-10-03 14:06:08 +0200

    Update
    Print tweaks
    Added offset (So 0,0,0 is in the middle of the transmitters)

--- data/test_calculations.m
+++ data/test_calculations.m
@@ -11,15 +11,19 @@ Dx=66358.54235;
 Dy=-69939.53471;
 Dz=39433.5018;
 
-testx=9;
-testy=100;
-testz=2000;
+origin_x=(Ax+Bx+Cx+Dx)/4;
+origin_y=(Ay+By+Cy+Dy)/4;
+origin_z=(Az+Bz+Cz+Dz)/4;
+
+testx=origin_x;
+testy=origin_y;
+testz=origin_z;
 
 dA=sqrt((testx-Ax)^2 + (testy-Ay)^2 + (testz-Az)^2);
 dB=sqrt((testx-Bx)^2 + (testy-By)^2 + (testz-Bz)^2);
 dC=sqrt((testx-Cx)^2 + (testy-Cy)^2 + (testz-Cz)^2);
 dD=sqrt((testx-Dx)^2 + (testy-Dy)^2 + (testz-Dz)^2);
 
-Xo = (dA^2 +Bx^2 -dB^2)/(2*Bx)
-Yo = ((Bx^2 +((dA^2 +Bx^2 -dB^2)/(2*Bx))*(2*Dx - 2*Bx) -dB^2 -Dx^2 -Dy^2 -Dz^2 +dD^2)/(-2*Dz) - (Bx^2 +((dA^2 +Bx^2 -dB^2)/(2*Bx))*(2*Cx - 2*Bx) -dB^2 -Cx^2 -Cy^2 -Cz^2 +dC^2)/(-2*Cz))/((2*Cy)/(-2*Cz)-(2*Dy)/(-2*Dz))
-Zo = ((Bx^2 +((dA^2 +Bx^2 -dB^2)/(2*Bx))*(2*Dx - 2*Bx) -dB^2 -Dx^2 -Dy^2 -Dz^2 +dD^2)/(-2*Dy) - (Bx^2 +((dA^2 +Bx^2 -dB^2)/(2*Bx))*(2*Cx - 2*Bx) -dB^2 -Cx^2 -Cy^2 -Cz^2 +dC^2)/(-2*Cy))/((2*Cz)/(-2*Cy)-(2*Dz)/(-2*Dy))
+Xo = (dA^2 +Bx^2 -dB^2)/(2*Bx)-origin_x
+Yo = ((Bx^2 +((dA^2 +Bx^2 -dB^2)/(2*Bx))*(2*Dx - 2*Bx) -dB^2 -Dx^2 -Dy^2 -Dz^2 +dD^2)/(-2*Dz) - (Bx^2 +((dA^2 +Bx^2 -dB^2)/(2*Bx))*(2*Cx - 2*Bx) -dB^2 -Cx^2 -Cy^2 -Cz^2 +dC^2)/(-2*Cz))/((2*Cy)/(-2*Cz)-(2*Dy)/(-2*Dz)) -origin_y
+Zo = ((Bx^2 +((dA^2 +Bx^2 -dB^2)/(2*Bx))*(2*Dx - 2*Bx) -dB^2 -Dx^2 -Dy^2 -Dz^2 +dD^2)/(-2*Dy) - (Bx^2 +((dA^2 +Bx^2 -dB^2)/(2*Bx))*(2*Cx - 2*Bx) -dB^2 -Cx^2 -Cy^2 -Cz^2 +dC^2)/(-2*Cy))/((2*Cz)/(-2*Cy)-(2*Dz)/(-2*Dy)) -origin_z
